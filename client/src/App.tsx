7dc7a35fbf1606d220c5478d132b0632572c04ea
Diego Giuliani <giuliani.diego@gmail.com>
2021-04-30 00:08:44 -0300

    Add Router config

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import HomePage from './views/home';
+import LoginPage from './views/login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="">Start code</header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/login" component={LoginPage} />
+        <Route path="/" component={HomePage} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ client/src/views/home.tsx
@@ -0,0 +1,9 @@
+import React, { ReactElement } from 'react';
+
+interface Props {}
+
+function HomePage({}: Props): ReactElement {
+  return <div>Home</div>;
+}
+
+export default HomePage;
