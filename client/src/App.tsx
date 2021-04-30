48335404dd97361f4f91d8becd9c7698664d1726
Diego Giuliani <giuliani.diego@gmail.com>
2021-04-29 23:54:49 -0300

    Initial Client setup

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Start code</header>
+    </div>
+  );
+}
+
+export default App;
