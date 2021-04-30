2f95065eab59b9323aae8a01c0e0a3f40a820136
Diego Giuliani <giuliani.diego@gmail.com>
2021-04-30 00:00:36 -0300

    Add tailwindcss config

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Start code</header>
+      <header className="">Start code</header>
     </div>
   );
 }
